ef409f44a09d0b948db1614015a01d63e342ac3d
pablo.echegaray <pablo.echegaray97@gmail.com>
2022-07-12 15:55:12 -0300

    temporizador 1 ciclo

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let duracion;
 let tiempo;
 let descansos;
-let desc=false;
+let desc=false; //indica si se esta descansando o no
 let inicio2;
-function init_desc() {
-    clearInterval(inicio);
+function init_desc() { // se inicializa cuando se termina el tiempo del pomodoro
+    clearInterval(inicio); //limpiamos la variable, terminando asi con el intervalo que llamaba a la funcion act_temporizador
     console.log("Inicio descanso");
-    tiempo = descansos * 60;
-    inicio2= setInterval(act_temporizador,1000);
-    desc=true;
+    tiempo = descansos * 60; // cambiar el valor de tiempo de manera global
+    inicio2= setInterval(act_temporizador,1000); // guardar en variable el intervalo para luego limpiarla cuando se termine el descanso
+    desc=true; // indica si se esta descansando
 }
 function act_temporizador() {
     let minutos = Math.floor(tiempo / 60); // devuelve el maximo entero 
@@ -17,13 +17,13 @@ function act_temporizador() {
         segundos = "0" + segundos;
     }
     console.log(minutos + ":" + segundos);
-    if (tiempo==0 && desc==false) {
+    if (tiempo==0 && desc==false) { //si se acaba el tiempo y no se esta descansando, iniciar descanso
         init_desc();
     }
     else {
-        tiempo--; //restar el tiempo total -> ej: 
-        if (desc==true  && minutos==0 && segundos==0) {
-            clearInterval(inicio2);
+        tiempo--; //restar 1 del tiempo total
+        if (desc==true  && minutos==0 && segundos==0) { //si se esta descansando y se acabaron los minutos y segundos
+            clearInterval(inicio2); //limpiamos la variable, terminando asi con el intervalo queu llamaba a la funcion act_temporizador
             console.log("Fin descanso");
         }
     }
@@ -42,7 +42,7 @@ tiempo = duracion * 60; //convertimos todo a segundos
 
 const inicio = setInterval(act_temporizador, 1000); // ejecutar la funcion act_temporizador cada un intervalo de 1000ms y guardarlo en la constante 'inicio' para luego limpiarla dependiendo la situacion
 //Pruebas 1er ciclo
-    // duracion = 2
+    // duracion = 2 (minutos)
     // tiempo = duracion * 2 -> = 120
     // llamar funcion cada un intervalo de 1000ms
     //en la funcion
@@ -50,7 +50,7 @@ const inicio = setInterval(act_temporizador, 1000); // ejecutar la funcion act_t
         // segundos = tiempo % 60 -> = 0 (el resto es 0 ya que duracion es igual a 120)
         // mostramos en consola el reloj -> 2:00
         // tiempo = tiempo -1 -> tiempo = 119
-        // si m=0 & s=0 salimos del intervalo limpiando la variable (este no es el caso), seguimos ejecutando la funcion cada un intervalo de 1000ms
+        // si tiempo=0 salimos del intervalo limpiando la variable (este no es el caso), seguimos ejecutando la funcion cada un intervalo de 1000ms
 //Pruebas 2er ciclo
     // llamar funcion cada un intervalo de 1000ms
     //en la funcion
@@ -58,4 +58,6 @@ const inicio = setInterval(act_temporizador, 1000); // ejecutar la funcion act_t
         // segundos = tiempo % 60 -> = 59 (el resto es 59 ya que duracion es igual a 119)
         // mostramos en consola el reloj -> 1:59
         // tiempo = tiempo -1 -> tiempo = 118
-        // si m=0 & s=0 salimos del intervalo limpiando la variable (este no es el caso), seguimos ejecutando la funcion cada un intervalo de 1000ms
+        // si tiempo=0 salimos del intervalo limpiando la variable (este no es el caso), seguimos ejecutando la funcion cada un intervalo de 1000ms
+//fin ciclo e inicio descanso
+    //si tiempo =0 se limpia la variable y se inicializa otra con el tiempo correspondiente a 
